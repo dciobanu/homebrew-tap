a39b3c7b21fe67ec7c52a5300f8e101c6f4c0470
dciobanu <dorin@ciobanu.org>
2026-09-10 13:43:48 -0700

    Update marksidian to 0.5.11

--- Casks/marksidian.rb
+++ Casks/marksidian.rb
@@ -1,9 +1,9 @@
 cask "marksidian" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.5.9"
-  sha256 arm:   "504e2dd1726e27994e1295b89cab21a3c5f774140cafafba2d017733dbb7c1ae",
-         intel: "6064bd6d2ed2864213a5002f2ffb1c2e833c2e99bd232813b220fe2bcb3e769b"
+  version "0.5.11"
+  sha256 arm:   "8f2ba4cb15e9449e5067936d5b714c80e8af147a061c3594eab8d5b38dc6553b",
+         intel: "443bcb76abbb389fb247fa2ebfdbddba77ceb11b154b33e656493adbf28e6136"
 
   url "https://github.com/dciobanu/marksidian-releases/releases/download/v#{version}/Marksidian-#{version}-#{arch}-mac.zip"
   name "Marksidian"
